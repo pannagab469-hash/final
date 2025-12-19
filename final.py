e17c9e7b21ede902c805d40cc13b9a38e573e0c2
pannagab469-hash <pannagab469@gmail.com>
2025-12-19 09:00:56 +0530

    Update final.py

--- final.py
+++ final.py
@@ -111,7 +111,7 @@ st.markdown("""
 # =================================================
 @st.cache_data
 def load_data():
-    df = pd.read_csv("social_media_engagement.csv")
+    df = pd.read_csv("social_media_engagement_enhanced(1).csv")
     df["date"] = pd.to_datetime(df["date"])
     return df
 
